3bb30eac034cd850b07727dcc5cca8186dc93860
Patrick Battisti Forsthofer <patrick.forsthofer@lisgo.me>
2021-09-18 23:42:24 +0100

    api url

--- src/util/api.ts
+++ src/util/api.ts
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const baseURL = 'http://192.168.43.104:3000';
+const baseURL = __DEV__
+  ? 'http://192.168.43.104:3000'
+  : 'http://167.71.26.220/fidelitas';
 
 const api = axios.create({
   baseURL,
